39a1a631613a527ace8b1a140f457d3eafd1c052
Matias Corteze <matiascorteze@gmail.com>
2020-10-13 16:19:08 -0300

    testeando commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/products" component={Products} />
+    <Route exact path="/contact" component={Contact} />
+  </Router>,
   document.getElementById('root')
 );
 
---
+++ src/pages/Products/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Products() {
+  return(
+    <h1>Soy el Products</h1>
+  )
+}
+
+export default Products
