8088d04ed3f7445c5b5ae517672ab39cdcba06f0
Matias Corteze <matiascorteze@gmail.com>
2020-10-28 15:58:59 -0300

    agregados botones de precios

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,20 +3,30 @@ import "./style.css"
 
 function Footer() {
   return (
-
     <section className="footer-container">
-      <div className="instagram">
-        <i className="fab fa-instagram" />
-        <span>@malapata3D</span>
+      <div className="footer">
+        <div className="instagram">
+          <i className="fab fa-instagram" />
+          <a
+            href="https://www.instagram.com/malapata3d/"
+            target="_blank"
+            rel="noopener noreferrer">@malapata3d</a>
+        </div>
+        <div className="mail">
+          <i className="far fa-envelope" />
+          <a
+            href="mailto:malapata3d@gmail.com&subject=Consulta%20web">malapata3d@gmail.com</a>
+        </div>
+        <div className="phone">
+          <i className="fas fa-phone-alt" />
+          <a href="https://wa.me/5491151743319">11-51743319</a>
+        </div>
       </div>
-      <div className="mail">
-        <i className="far fa-envelope" />
-        <span>info@malapata3d.com</span>
-      </div>
-      <div className="phone">
-        <i className="fas fa-phone-alt" />
-        <span>011-12345678</span>
-      </div>
+      <a 
+        href="https://github.com/matiascorteze"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="signature">Made with <span className="heart">♥</span> by Matias Corteze</a>
     </section>
   )
 }
